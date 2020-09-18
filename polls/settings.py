6f08db81e47850da6d757ee576e086ae9abf8172
Wesin Alves <wesinec@gmail.com>
2020-09-18 13:46:18 -0300

    add models

--- polls/settings.py
+++ polls/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'poll.apps.PollConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_seed',
 ]
 
 MIDDLEWARE = [
