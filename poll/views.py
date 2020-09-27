b6d8a1ed86d1dad7a414fab22779981ad082ce64
WesinAlves <wesinec@gmail.com>
2020-09-27 11:10:32 -0300

    add filter to index view

--- poll/views.py
+++ poll/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 from .models import Choice, Question
@@ -11,7 +12,13 @@ class IndexView(generic.ListView):
     template_name = 'poll/index.html'
     context_object_name = 'latest_question_list'
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:15]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte = timezone.now()
+        ).order_by('-pub_date')[:15]
 
 class DetailView(generic.DetailView):
     model = Question
