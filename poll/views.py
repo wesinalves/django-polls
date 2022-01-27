74e7c37c1a08b0373243f545bef1594ccff2ea24
wesinalves <wesinec@gmail.com>
2022-01-27 11:56:16 -0300

    add new changes

--- poll/views.py
+++ poll/views.py
@@ -56,3 +56,8 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
+
+def report(request):
+    """Página inicial do aplicativo"""
+    return render(request, 'poll/report.html')
+
